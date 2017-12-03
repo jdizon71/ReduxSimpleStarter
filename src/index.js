aee9bf9906edd1e4f9b09bbeaeb995b642c7c73c
jdizon71 <justdizon71@gmail.com>
2017-12-03 12:45:56 -0800

    :art: Creates base searchBar component and exports/imports searchBar component into App component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'; // ES6 import statement
 import ReactDOM from 'react-dom'; // Used to render a component to the DOM
 
-const API_KEY = '';
+import SearchBar from './components/searchBar';
+
+const API_KEY = 'AIzaSyAMv4EEYImFMpRPIIG6AiEozrWFsWmncU8';
 
 // Create a new component
 // This component should generate some HTML
 
 const App = () => { // ES6 syntax replaces `function` keyword with fat arrow
-  return <div>Hi!</div>;
-}
+  return ( // For multiline JSX, it's best, but not required to use () to encapsulate the entire expression
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 // After creating this component, we need to insert the component's generated HTML onto the page (DOM)
 ReactDOM.render(<App />, document.querySelector(".container"));
