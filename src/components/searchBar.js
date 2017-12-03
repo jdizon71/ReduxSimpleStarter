6b9a476165c7e12133845df8e4ffa77df6f92130
jdizon71 <justdizon71@gmail.com>
2017-12-03 13:32:06 -0800

    :art: Adds event handling for user input in SearchBar component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component { // extends React.Component gives SearchBar functionality from React.Component class
   render() { // syntax for defining methods on a class
-    return <input />;
+    return <input onChange = { event => console.log(event.target.value) } />; // accesses what the user types within the <input /> and logs it to the console
   }
+
 }
 
-// const SearchBar = () => {
+// const SearchBar = () => { // functional component syntax
 //   return <input />;
 // };
 
