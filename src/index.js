9d7032cd34d32d2e49ac1adc574695b25464ddd1
jdizon71 <justdizon71@gmail.com>
2017-12-02 20:09:44 -0800

    :art: Refactors code to create/render new HTML element to the DOM

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'; // ES6 import statement
+import ReactDOM from 'react-dom'; // Used to render a component to the DOM
+
+
+// Create a new component
+// This component should generate some HTML
+
+const App = () => { // ES6 syntax replaces `function` keyword with fat arrow
+  return <div>Hi!</div>;
+}
+
+// After creating this component, we need to insert the component's generated HTML onto the page (DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
