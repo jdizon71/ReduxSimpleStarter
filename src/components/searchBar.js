a84fbd73a7b018174ebfec58a3ebb5b696cda292
jdizon71 <justdizon71@gmail.com>
2017-12-03 14:23:26 -0800

    :art: Updates function to initialize/store/update state

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component { // extends React.Component gives SearchBar functionality from React.Component class
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() { // syntax for defining methods on a class
-    return <input onChange = { event => console.log(event.target.value) } />; // accesses what the user types within the <input /> and logs it to the console
+    return (
+      <div>
+        <input onChange = { event => this.setState({ term: event.target.value }) } />
+        Value of the input: { this.state.term }
+      </div>
+    );
   }
 
 }
