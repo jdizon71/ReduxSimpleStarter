64081056768afd0e20565e2144329888109bf613
jdizon71 <justdizon71@gmail.com>
2017-12-04 00:58:32 -0800

    :art: Creates VideoListItem component and connects to VideoList component

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import VideoListItem from './videoListItem';
+
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+      return <VideoListItem video={ video } />
+  });
+
+// React will recognize `videoItems` is an array and will render each element
+
   return (
     <ul className="col-md-4 list-group">
-      { props.videos.length }
+      { videoItems }
     </ul>
   );
 };
