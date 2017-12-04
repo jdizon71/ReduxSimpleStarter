d1a99fb33896b2f2ef8a2a65978b3027f0a37262
Justin Dizon <justin.dizon@uber.com>
2017-12-04 08:46:31 -0800

    :art: Adds unique identifier for each VideoListItem

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -4,7 +4,7 @@ import VideoListItem from './videoListItem';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-      return <VideoListItem video={ video } />
+      return <VideoListItem  key={ video.etag } video={ video } />
   });
 
 // React will recognize `videoItems` is an array and will render each element
