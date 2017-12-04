bc3616981a0156e1cd53c1666a0107c6f03c9aa8
jdizon71 <justdizon71@gmail.com>
2017-12-03 20:34:14 -0800

    :art: Converts App component from functional to class-based. Adds YouTube Search API call to App component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'; // ES6 import statement
+import React, { Component } from 'react'; // ES6 import statement
 import ReactDOM from 'react-dom'; // Used to render a component to the DOM
 import YTSearch from 'youtube-api-search';
 
@@ -6,17 +6,27 @@ import SearchBar from './components/searchBar';
 
 const API_KEY = 'AIzaSyAMv4EEYImFMpRPIIG6AiEozrWFsWmncU8';
 
-YTSearch({ key: API_KEY, term: 'boosted board' }, (data) => console.log(data) );
-
 // Create a new component
 // This component should generate some HTML
 
-const App = () => { // ES6 syntax replaces `function` keyword with fat arrow
-  return ( // For multiline JSX, it's best, but not required to use () to encapsulate the entire expression
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component { // ES6 syntax replaces `function` keyword with fat arrow
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({ key: API_KEY, term: 'boosted board' }, (videos) => { // `video` is arbitrary; you can whatever variable name you want
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return ( // For multiline JSX, it's best, but not required to use () to encapsulate the entire expression
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 };
 
 // After creating this component, we need to insert the component's generated HTML onto the page (DOM)
